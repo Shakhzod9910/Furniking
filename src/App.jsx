e614fc5d37496cb8a151e84963e86e53324a10b3
Shakhzod9910 <92552024+Shakhzod9910@users.noreply.github.com>
2022-01-28 23:07:40 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import React, { useContext, useState } from 'react'
+import Header from './components/header/heder';
+import Home from './components/Home/Home';
+import Shop from './components/Shop/Shop';
+import Footer from './components/Footer/Footer';
+function App() {
+  return (
+    <>
+      <Header />
+     
+    </>
+  );
+}
+
+export default App;
